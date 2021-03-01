ba3001397695e3f124cad7ae352b68389c6ff5cf
michaelpetty <michael.petty@gmail.com>
2021-02-28 21:59:54 -0800

    init connection w socket.io between client and server

--- api/views/app/main.js
+++ api/views/app/main.js
@@ -11,9 +11,21 @@ new Vue({
       this.modal.state = 'ANSWER'
     },
   },
-  data() { 
+  created() {
+    console.log('in the created method')
+    this.socket = io()
+  },
+  mounted() {
+    console.log('in the mounted method')
+    this.socket.on('hello', data => {
+      console.log('mounted data from socket: ', data)
+      this.socket.emit('hello', `i see your ${data} and raise you a Galaxy!`)
+    })
+  },
+  data() {
+    console.log('in data method')
     return {
-      modal: { 
+      modal: {
         state: 'CLOSED',
         category: null,
         value: null,
@@ -22,11 +34,11 @@ new Vue({
       },
       categories: [ "General", "Data Types", "Control Flow", "Functions", "Objects & Arrays" ],
       board: [
-        [ 
+        [
           {
             category: "General",
             answered: false,
-            value: "$200", 
+            value: "$200",
             answer: "A reference in code that holds a value.",
             question: "What is a variable?",
           },
@@ -40,30 +52,30 @@ new Vue({
           {
             category: "Control Flow",
             answered: false,
-            value: "$200", 
+            value: "$200",
             answer: "This loop will run so long as a general condition is true",
             question: "What is a while loop?"
           },
           {
             category: "Functions",
             answered: false,
-            value: "$200", 
+            value: "$200",
             answer: "A value that is passed into a funciton",
             question: "What is an argument?"
           },
           {
             category: "Objects & Arrays",
             answered: false,
-            value: "$200", 
+            value: "$200",
             answer: "The position of an item in an array",
             question: "What is an index?"
           },
         ],
-        [ 
+        [
           {
             category: "General",
             answered: false,
-            value: "$400", 
+            value: "$400",
             answer: "The language used to add behavior to a web page",
             question: "What is JavaScript?"
           },
@@ -77,30 +89,30 @@ new Vue({
           {
             category: "Control Flow",
             answered: false,
-            value: "$400", 
+            value: "$400",
             answer: "This loop will run for a set number of iterations",
             question: "What is a for loop?"
           },
           {
             category: "Functions",
             answered: false,
-            value: "$400", 
+            value: "$400",
             answer: "This statement outputs a single value from a function",
             question: "What is a return statement?"
           },
           {
             category: "Objects & Arrays",
             answered: false,
-            value: "$400", 
+            value: "$400",
             answer: "This function removes the last element from an array",
             question: "What is the `pop()` function?"
           },
         ],
-        [ 
+        [
           {
             category: "General",
             answered: false,
-            value: "$600", 
+            value: "$600",
             answer: "The syntax for a single-line comment in JavaScript",
             question: "What are two slashes?"
           },
@@ -114,30 +126,30 @@ new Vue({
           {
             category: "Control Flow",
             answered: false,
-            value: "$600", 
+            value: "$600",
             answer: "This performs a loose, not strict, equality comparison",
             question: "What are two equal signs?"
           },
           {
             category: "Functions",
             answered: false,
-            value: "$600", 
+            value: "$600",
             answer: "A variable that exists only within the body of a function",
             question: "What is a local variable?"
           },
           {
             category: "Objects & Arrays",
             answered: false,
-            value: "$600", 
+            value: "$600",
             answer: "The succinct notation for accessing a value stored in an object",
             question: "What is dot notation?"
           },
         ],
-        [ 
+        [
           {
             category: "General",
             answered: false,
-            value: "$800", 
+            value: "$800",
             answer: "When a computer doesn't know how to proceed execution",
             question: "What is an error?"
           },
@@ -151,58 +163,58 @@ new Vue({
           {
             category: "Control Flow",
             answered: false,
-            value: "$800", 
+            value: "$800",
             answer: "This statement can take the place of a very large `if` statement",
             question: "What is a `switch` statement?"
           },
           {
             category: "Functions",
             answered: false,
-            value: "$800", 
+            value: "$800",
             answer: "The default return value of a function",
             question: "What is `undefined`?"
           },
           {
             category: "Objects & Arrays",
             answered: false,
-            value: "$800", 
+            value: "$800",
             answer: "Short for \"JavaScript Object Notation\"",
             question: "What does JSON stand for?"
           },
         ],
-        [ 
+        [
           {
             category: "General",
             answered: false,
-            value: "$1000", 
+            value: "$1000",
             answer: "When someone breaks down problems before writing code",
             question: "What is pseudocoding?"
           },
           {
             category: "Data Types",
             answered: false,
-            value: "$1000", 
+            value: "$1000",
             answer: "This keyword prevents reassignment",
             question: "What is `const`?"
           },
           {
             category: "Control Flow",
             answered: false,
-            value: "$1000", 
+            value: "$1000",
             answer: "An expression that branches, similar to the `if` statement",
             question: "What is a ternary?"
           },
           {
             category: "Functions",
             answered: false,
-            value: "$1000", 
+            value: "$1000",
             answer: "What determines where in our code a variable can be accessed",
             question: "What is scope?"
           },
           {
             category: "Objects & Arrays",
             answered: false,
-            value: "$1000", 
+            value: "$1000",
             answer: "The object where `pop`, `push`, `map` and more are located",
             question: "What is `Array.prototype`?"
           },
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 const db = require('./api/models')
 
 const PORT = process.env.PORT || 8000
 const rootViews = __dirname + '/api/views'
 
 app.use(express.static(rootViews))
+
+io.on('connection', socket => {
+  socket.emit('hello', 'world!')
+
+  socket.on('hello', arg => {
+    console.log(arg)
+  })
+})
+
+app.get('/', (req,res) => {
+  console.log('serving root')
+  res.sendFile(rootViews + '/app/index.html')
+})
 
 app.get('/answers', async (req, res) => {
   try {
@@ -18,4 +33,4 @@ app.get('/answers', async (req, res) => {
 
 })
 
-app.listen(PORT, () => console.log(`Up and running on ${PORT}`))
+server.listen(PORT, () => console.log(`Up and running on ${PORT}`))
